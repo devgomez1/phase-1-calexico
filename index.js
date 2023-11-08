8b281d1f37b35d1cb1c3976e77f5aef9e56d1e15
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-08 11:32:56 -0600

    finished core deliverables

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here...
 fetch("http://localhost:3000/menu")
   .then((response) => response.json())
-  .then((data) => data.forEach(displayMenuItems, console.log(data)));
+  .then((data) => {
+    console.log(data);
+    menuDetails(data[0]);
+    data.forEach(displayMenuItems);
+  });
 
 function displayMenuItems(item) {
   let menuDiv = document.getElementById("menu-items");
@@ -10,16 +14,30 @@ function displayMenuItems(item) {
   menuDiv.append(menuSpan);
 
   menuSpan.addEventListener("click", function () {
-    let img = document.getElementById("dish-image");
-    img.src = item.image;
-
-    let name = document.getElementById("dish-name");
-    name.textContent = item.name;
-
-    let info = document.getElementById("dish-description");
-    info.textContent = item.description;
-
-    let price = document.getElementById("dish-price");
-    price.textContent = item.price;
+    menuDetails(item);
   });
 }
+
+function menuDetails(item) {
+  let img = document.getElementById("dish-image");
+  img.src = item.image;
+
+  let name = document.getElementById("dish-name");
+  name.textContent = item.name;
+
+  let info = document.getElementById("dish-description");
+  info.textContent = item.description;
+
+  let price = document.getElementById("dish-price");
+  price.textContent = item.price;
+}
+
+let form = document.getElementById("cart-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let number = document.getElementById("number-in-cart");
+  let currentNumber = Number(number.textContent);
+  let amount = Number(e.target["cart-amount"].value);
+
+  number.textContent = amount + currentNumber;
+});
