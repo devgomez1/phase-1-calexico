a14d1c48f86b8c3bdcbb0a526a45dda226496b5f
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-08 10:35:56 -0600

    added details

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here...
+fetch("http://localhost:3000/menu")
+  .then((response) => response.json())
+  .then((data) => data.forEach(displayMenuItems, console.log(data)));
+
+function displayMenuItems(item) {
+  let menuDiv = document.getElementById("menu-items");
+  let menuSpan = document.createElement("span");
+  menuSpan.textContent = item.name;
+  menuDiv.append(menuSpan);
+
+  menuSpan.addEventListener("click", function () {
+    let img = document.getElementById("dish-image");
+    img.src = item.image;
+
+    let name = document.getElementById("dish-name");
+    name.textContent = item.name;
+
+    let info = document.getElementById("dish-description");
+    info.textContent = item.description;
+
+    let price = document.getElementById("dish-price");
+    price.textContent = item.price;
+  });
+}
